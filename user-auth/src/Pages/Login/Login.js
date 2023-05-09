d6dd9fa83ae0da9c51c567e86f731b050ac29903
Monte <monte6198@gmail.com>
2023-05-09 10:32:05 +0900

    feat: 정규표현식을 이용한 이메일 및 비밀번호 유효성 검사

--- user-auth/src/Pages/Login/Login.js
+++ user-auth/src/Pages/Login/Login.js
@@ -6,24 +6,36 @@ const Login = () => {
   // state
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
+  const [emailValidation, setEmailValidation] = useState(false);
+
   const [password, setPassword] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [passwordValidation, setPasswordValidation] = useState(false);
+
+  const emailRegex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])",
+  );
+  const passwordRegex = new RegExp(
+    '/^.*(?=^.{8,15}$)(?=.*d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/',
+  );
 
   // event handlers
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
-    if (email.length < 6) {
-      setEmailError('Email length is too short');
+    if (!emailRegex.test(email)) {
+      setEmailError('Email is not valid');
     } else {
       setEmailError('');
+      setEmailValidation(true);
     }
   };
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-    if (password.length < 6) {
-      setPasswordError('Password should be at least 8 letters');
+    if (!passwordRegex.test(password)) {
+      setPasswordError('Password is not valid');
     } else {
       setPasswordError('');
+      setPasswordValidation(true);
     }
   };
   const handleLoginClick = () => {
@@ -64,6 +76,7 @@ const Login = () => {
             <p className='error-message'>{passwordError}</p>
           )}
           <button
+            disabled={emailValidation || passwordValidation}
             className='user-input login-button'
             onClick={handleLoginClick}
           >
